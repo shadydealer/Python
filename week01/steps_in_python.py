76baa4678aaf8b13d8042ecd8438f4cf52e56e18
Shady Dealer <shady>
2018-03-30 09:16:28 +0300

    refactored some functions.

--- week01/steps_in_python.py
+++ week01/steps_in_python.py
@@ -5,31 +5,37 @@ def sum_of_digits(n):
 	sum = 0	
 	while n > 0 :
 		sum += (n%10)
-		n/=10
-	print(sum)
+		n//=10
+	return sum
 
 def to_digits(n):
 	arr = []
+	if n == 0:
+		arr.append(0)
 	while n > 0 :
 		arr.append(n%10)
 		n//=10
 	arr.reverse()
-	print(arr)
+	return arr
 	
 def to_number(digits):
 	num =0
 	for digit in digits:
 		num *=10
 		num += digit
-	print (num)
+	return num
 
-def sum_of_factorials(n):
+	
+def fact_digits(n):
+	sum = 0
 	n = abs(n)
-	sum = 0	
+	if n == 0:
+		sum = 1 
 	while n > 0 :
-		sum += (math.factorial(n%10))
-		n/=10
-	print(sum)
+		print(n)
+		sum += (math.factorial((n%10)))
+		n//=10
+	return sum
 
 def fibonacci(n):
 	arr = []
@@ -41,21 +47,14 @@ def fibonacci(n):
 			arr.append(arr[i] + temp)
 			temp = arr[i]
 			i+=1
-	print(arr)
+	return arr
 
 def fib_number(n):
-	arr = []
-	temp = 0
-	i = 0
-	if n >= 1:
-		arr.append(1)
-		while len(arr) <= n:
-			print(arr[i], end = '')
-			arr.append(arr[i] + temp)
-			temp = arr[i]
-			i+=1
-	print()
-
+	arr = fibonacci(n)
+	result = ''
+	for num in arr:
+		result += str(num)
+	return result
 
 def palindrome(n):
 	temp = str(n)
@@ -71,32 +70,22 @@ def count_vowels(string):
 	string = string.lower()
 	vowels = "aeiouy"
 	counter = 0
-	i = 0
-	while i < len(string):
-		j = 0
-		while j < len(vowels):
-			if string[i] == vowels[j]:
-				counter +=1
-				break
-			j+=1
-		i +=1
-	print(counter)
+	
+	for char in string:
+		if char in vowels:
+			counter+=1
+	
+	return counter
 
 def count_consonants(string):
 	string = string.lower()
-	vowels = "aeiouy"
+	consonants = "bcdfghjklmnpqrstvwxz"
 	counter = 0
-	i = 0
-	while i < len(string):
-		j = 0
-		while j < len(vowels):
-			if string[i] == vowels[j]:
-				break
-			j+=1
-		if j == len(vowels):
-			counter +=1
-		i +=1
-	print(counter)
+
+	for char in string:
+		if char in consonants:
+			counter+=1
+	return counter
 
 def char_histogram(string):
 	hist = {}
@@ -105,18 +94,18 @@ def char_histogram(string):
 			hist.update({c:1})
 		else:
 			hist[c] +=1
-	print(hist)
+	return hist
 
-num = int(input("Please enter a number: "))
-sum_of_digits(num)
-to_digits(num)
-fibonacci(num)
-sum_of_factorials(num);
-fib_number(num)
-to_number([1,2,3,0,2,3])
-
-text = input()
-print(palindrome(text))
-count_vowels(text)
-count_consonants(text)
-char_histogram(text)
+#num = 20
+#sum_of_digits(num)
+print(to_digits(123))
+#fibonacci(num)
+#print(fact_digits(111))
+#print(fib_number(5))
+#to_number([1,2,3,0,2,3])
+#
+#text = input()
+#print(palindrome(text))
+#count_vowels(text)
+#count_consonants(text)
+#char_histogram(text)

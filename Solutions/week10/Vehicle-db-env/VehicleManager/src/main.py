b64a6c7166d633b3ef624411327e0cfb1758c5c9
Shady Dealer <dimiturlukanov@gmail.com>
2018-05-17 22:23:34 +0300

    Teststing out logging in and adding to db.

--- Solutions/week10/Vehicle-db-env/VehicleManager/src/main.py
+++ Solutions/week10/Vehicle-db-env/VehicleManager/src/main.py
@@ -1,4 +1,16 @@
+from controllers.account_controller import AccountController
 
+CURRENT_DB_NAME = 'queries/database/vehicle_management.db'
 def main():
-    pass
-  
+    USERNAME = "wow"
+    PASSWORD = "PASSWORD"
+    accountController= AccountController(dbName=CURRENT_DB_NAME)
+    accountController.login(username=USERNAME, password=PASSWORD)
+
+    accountController.create_account(userType="client",username="wow", password="PASSWORD",
+                                     email="haHAA@gmail.com", phoneNumber=912848912,
+                                     address="Nowhere")
+
+
+if __name__ == '__main__':
+    main()
